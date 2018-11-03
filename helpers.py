441476dd3ad2f061ca088df7b420292df5d3a4f0
Charles McEachern <charles.a.mceachern@gmail.com>
2018-11-03 18:39:47 -0500

    add direction sensor

--- helpers.py
+++ helpers.py
@@ -6,37 +6,38 @@ import neopixel
 # ======================================================================
 
 def cpe_log_show(vmin, vmax, v):
-    return cpe_neopixel_show( log_to_ten(vmin, vmax, v) )
+    return cpe_neopixel_scale( log_to_ten(vmin, vmax, v) )
 
 # ----------------------------------------------------------------------
 
 def cpe_lin_show(vmin, vmax, v):
-    return cpe_neopixel_show( lin_to_ten(vmin, vmax, v) )
+    return cpe_neopixel_scale( lin_to_ten(vmin, vmax, v) )
 
 # ----------------------------------------------------------------------
 
 NEOPIXEL = neopixel.NeoPixel(board.NEOPIXEL, 10, brightness=0.1, auto_write=False)
 
-def cpe_neopixel_show(n):
+COLORS = [
+    (0, 0, 255),
+    (0, 64, 192),
+    (0, 128, 128),
+    (0, 192, 64),
+    (0, 255, 0),
+    (64, 192, 0),
+    (128, 128, 0),
+    (192, 64, 0),
+    (255, 0, 0),
+    (192, 0, 64),
+]
+
+BLACK = (0, 0, 0)
+
+def cpe_neopixel_scale(n):
     """Accept a number from 0 to 10. Light up that many LEDs."""
-    black = (0, 0, 0)
-    rainbow = [
-        (0, 0, 255),
-        (0, 64, 192),
-        (0, 128, 128),
-        (0, 192, 64),
-        (0, 255, 0),
-        (64, 192, 0),
-        (128, 128, 0),
-        (192, 64, 0),
-        (255, 0, 0),
-        (192, 0, 64),
-    ]
     for i in range(10):
         # Gross, they're ordered counterclockwise.
-        NEOPIXEL[9 - i] = black if i >= int(n) else rainbow[i]
-        NEOPIXEL.show()
-    return
+        NEOPIXEL[9 - i] = BLACK if i >= int(n) else COLORS[i]
+    return NEOPIXEL.show()
 
 # ----------------------------------------------------------------------
 
