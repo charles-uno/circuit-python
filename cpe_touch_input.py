80b7fa8c6c0716a999c4cde982250b285b6d7e13
Charles McEachern <charles.a.mceachern@gmail.com>
2018-11-03 21:31:04 -0500

    clarify comment

--- cpe_touch_input.py
+++ cpe_touch_input.py
@@ -8,6 +8,7 @@ import helpers
 # ======================================================================
 
 TOUCHES = [
+    # touchio.TouchIn(whatever).value is true if it's being touched.
     touchio.TouchIn(board.A1),
     touchio.TouchIn(board.A2),
     touchio.TouchIn(board.A3),
@@ -17,8 +18,6 @@ def cpe_touch_input():
     """Touch A1, A2, and A3 to make colors race."""
     while True:
         for channel, touch in enumerate(TOUCHES):
-            # touchio.TouchIn(board.A1) is True if A1 is being touched,
-            # False otherwise. 
             if touch.value:
                 helpers.chase_advance(channel)
             else:
